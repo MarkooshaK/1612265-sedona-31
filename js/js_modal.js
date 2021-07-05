002745e5779be01fa3d8fbd9a7ba96d860f35663
MarkooshaK <marinakokh99@gmail.com>
2021-07-06 01:14:03 +0300

    js и мелкие правки

--- js/js_modal.js
+++ js/js_modal.js
@@ -1,13 +1,36 @@
 let button = document.querySelector(".button__search");
-let popup = document.querySelector(".popap");
+let popup = document.querySelector(".popup");
 let adults_decrease = document.querySelector(".button-adults-less");
 let adults_increase = document.querySelector(".button-adults-more");
 let childs_decrease = document.querySelector(".button-childs-less");
 let childs_increase = document.querySelector(".button-childs-more");
+let form = document.querySelector(".popup__form");
+let arrival = document.querySelector("[name=arrival]");
+let departure = document.querySelector("[name=departure]");
 
 button.addEventListener("click", function(evt) {
   evt.preventDefault();
-  popup.classList.toggle("visually-hidden");
+  popup.classList.toggle("popup__hidden");
+  popup.classList.remove("form__error");
+});
+
+button.addEventListener("click", function(evt) {
+  if(popup.classList.contains("popup__hidden")) {
+    evt.preventDefault();
+  }
+  else {
+    popup.classList.add("popup__animation");
+  }
+});
+
+form.addEventListener("submit", function(evt){
+  if(!arrival.value || !departure.value) {
+    evt.preventDefault();
+    popup.classList.add("form__error");
+    setTimeout(function(){
+      popup.classList.remove("form__error");
+    }, 500);
+  }
 });
 
 adults_increase.addEventListener("click", function(adults) {
@@ -39,6 +62,8 @@ childs_decrease.addEventListener("click", function(childs) {
   value--;
   document.getElementById("number-of-childs").value = value;
 });
+
+
 
 
 
